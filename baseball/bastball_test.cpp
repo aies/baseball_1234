b7dd1dec05ff0b8cef7d0843a98eb61b3e7ef984
ies.cho <ies.cho@sec.com>
2025-07-23 13:53:26 +0900

    [refactor] TC2

--- baseball/bastball_test.cpp
+++ baseball/bastball_test.cpp
@@ -42,7 +42,7 @@ TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber2) {
     GuessResult result = game.guess("321");
 
     EXPECT_FALSE(result.solved);
-    EXPECT_EQ(2, result.strikes);
+    EXPECT_EQ(1, result.strikes);
     EXPECT_EQ(2, result.balls);
 }
 
