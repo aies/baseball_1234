6336e51cf4afaab71780fa50d664080e5a797395
ies.cho <ies.cho@sec.com>
2025-07-23 12:56:30 +0900

    [feature] Check invalid argument

--- baseball/baseball.cpp
+++ baseball/baseball.cpp
@@ -4,7 +4,13 @@ using namespace std;
 class Baseball {
 public:
     void guss(const string& string) {
-        if (string.length() != 3)
+        if (string.length() != 3) {
             throw length_error("Must be three letters.");
+        }
+        for (char ch : string) {
+            if (ch < '0' || ch > '9') {
+                throw invalid_argument("Must be number.");
+            }
+        }
     }
 };
--- baseball/bastball_test.cpp
+++ baseball/bastball_test.cpp
@@ -7,6 +7,11 @@ TEST(baseballGame, TryGameTest) {
     EXPECT_THROW(game.guss(string("12")), length_error);
 }
 
+TEST(baseballGame, ThrowExceptionWhenInvalidChar) {
+    Baseball game;
+
+    EXPECT_THROW(game.guss(string("12s")), invalid_argument);
+}
 
 int main() {
     ::testing::InitGoogleMock();
