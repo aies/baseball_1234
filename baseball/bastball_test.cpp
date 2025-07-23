580068619706c6b9747bef26a97707fbc1984bbe
ies.cho <ies.cho@sec.com>
2025-07-23 13:02:00 +0900

    [refactor] Make test fixture

--- baseball/bastball_test.cpp
+++ baseball/bastball_test.cpp
@@ -1,16 +1,24 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
+class BaseballFixture : public testing::Test {
+    Baseball game;
+
+    void assertIllegalArgment(string gussNumber) {
+        try {
+            game.guss(string("12s"));
+            FAIL();
+        }
+        catch (exception e) {
+            //PASS
+        }
+    }
+};
+
 TEST(baseballGame, TryGameTest) {
     Baseball game;
 
     EXPECT_THROW(game.guss(string("12")), length_error);
-}
-
-TEST(baseballGame, ThrowExceptionWhenInvalidChar) {
-    Baseball game;
-
-    EXPECT_THROW(game.guss(string("12s")), invalid_argument);
 }
 
 int main() {
